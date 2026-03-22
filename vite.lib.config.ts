3fa6f0b3a550209f4d1feee175454194a4fad2c1
ms-sk <39585579+ms-sk@users.noreply.github.com>
2026-03-22 21:46:12 +0100

    Update vite.lib.config.ts
    #1 config and package

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -1,26 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'ReactLanguagePicker',
-      fileName: (format) => `react-language-combobox.${format}.js`,
-      formats: ['es', 'cjs', 'umd']
+      // Hier den Pfad zu deiner Hauptdatei (z.B. index.ts oder LanguagePicker.tsx)
+      entry: resolve(__dirname, 'src/index.ts'), 
+      name: 'ReactLanguageCombobox',
+      fileName: (format) => `react-language-combobox.${format === 'es' ? 'es' : 'cjs'}.js`,
+      formats: ['es', 'cjs']
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: [
+        'react', 
+        'react-dom', 
+        'react/jsx-runtime', 
+        'i18next', 
+        'react-i18next',
+        'tailwindcss'
+      ],
       output: {
         globals: {
           react: 'React',
-          'react-dom': 'ReactDOM'
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime',
+          'react-i18next': 'reactI18next'
         }
       }
-    },
-    emptyOutDir: true,
-    outDir: 'dist'
+    }
   }
-})
+});
